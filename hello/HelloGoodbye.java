9b936c34b67bfec575325f6b220f10e16b7185f9
Huot Rithy <110338150+Liqaing@users.noreply.github.com>
2023-12-11 22:07:46 +0700

    Course Introduction, Hello, World assignment

---
+++ hello/HelloGoodbye.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        if (args.length >= 2) {
+            System.out.println("Hello " + args[0] + " and " + args[1]);
+            System.out.println("Goodbye " + args[1] + " and " + args[0]);
+        }
+    }
+}
